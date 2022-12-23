90dbaced6b1f7fbbfd06f5813bd62896ff647bb4
Marwin5411-Winner <st.winnerrop@gmail.com>
2022-12-23 08:48:29 +0700

    update Cart, Order
    Cart
    Order System

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,44 @@ const userSchema = require('../schema/users');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.session.user) {
-  res.render('index', { title: 'Express', user: req.session.user });
+    const products = [
+      {
+        id: 1,
+        name: 'Product 1',
+        price: 100,
+        image: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        price: 200,
+        image: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 3,
+        name: 'Product 3',
+        price: 300,
+        image: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 4,
+        name: 'Product 4',
+        price: 400,
+        image: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 5,
+        name: 'Product 5',
+        price: 500,
+        image: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 6,
+        name: 'Product 6',
+        price: 600,
+        image: 'https://picsum.photos/200/300'
+      }];
+  res.render('index', { title: 'Express', user: req.session.user, products : products });
   } else {
     res.redirect('/login');
   }
--- routes/order.js
+++ routes/order.js
@@ -1,42 +1,118 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 100,
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 200,
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 300,
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: 400,
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    price: 500,
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    price: 600,
+    image: "https://picsum.photos/200/300",
+  },
+];
 
-
-router.get('/', (req, res) => {
-    if (req.session.name && req.session.pin) {   
-        res.render('cart', { title: 'Cart' });  
+router.get("/", (req, res) => {
+  if (req.session.user) {
+    //calculate total
+    
+    //convert array string to number
+    req.session.order = req.session.order.map((item) => parseInt(item));
+    let total = 0;
+    if (req.session.order) {
+        req.session.order.forEach((item) => {
+        const product = products.find((product) => product.id == item);
+        total += product.price;
+        console.log(total);
+        });
     }
-})
 
-router.post('/', (req, res) => {
-})
+    res.render("cart", { title: "Cart", user: req.session.user, order: req.session.order, products: products , total: total});
+  } else {
+    res.redirect("/login");
+  }
+});
 
-//Add order to cart and redirect to / 
-router.post('/add', (req, res) => {
-    if (req.session.user) {
-        if (req.body) {
-            req.session.order = req.body
-            res.redirect('/')
-        } else {
-            res.redirect('/cart')
-        }
+router.post("/", (req, res) => {});
+
+router.get("/add/:id", (req, res) => {
+  if (req.session.user) {
+    if (req.params.id) {
+      if (req.session.order) {
+        req.session.order.push(req.params.id);
+      } else {
+        req.session.order = [req.params.id];
+      }
+      res.redirect("/");
     } else {
-        res.redirect('/login')
+      res.redirect("/cart");
     }
-})
+  } else {
+    res.redirect("/login");
+  }
+});
 
-router.get('/add/:id', (req, res) => {
-    if (req.session.user) {
-        if (req.params.id) {
-            req.session.order = req.params.id
-            res.redirect('/')
-        } else {
-            res.redirect('/cart')
-        }
+router.get("/remove/:id", (req, res) => {
+  if (req.session.user) {
+    if (req.params.id) {
+      if (req.session.order) {
+        req.session.order = req.session.order.filter(
+          (item) => item !== req.params.id
+        );
+      }
+      res.redirect("/cart");
     } else {
-        res.redirect('/login')
+      res.redirect("/cart");
     }
-})
+  } else {
+    res.redirect("/login");
+  }
+});
 
+router.get("/clear", (req, res) => {
+  if (req.session.user) {
+    if (req.session.order) {
+      req.session.order = [];
+    }
+    res.redirect("/cart");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/getSession", (req, res) => {
+  if (req.session.user) {
+    res.send(req.session.order);
+  } else {
+    res.redirect("/login");
+  }
+});
 
 module.exports = router;
